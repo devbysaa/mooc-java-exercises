c22b0703a723af2ec75f88b7e700282af1f53c82
saa senthil <hello@writetosaa.com>
2025-06-10 09:54:10 +1000

    completed exercises 1 - 4 for java programming i, part 1

---
+++ java-programming-i/part-1/3-once-upon-a-time/src/main/java/OnceUponATime.java
@@ -0,0 +1,11 @@
+
+public class OnceUponATime {
+
+    public static void main(String[] args) {
+
+        // Prints out the following 3 statements to the console
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
+    }
+}
